034d04200d58e7a8531f40c843636bc52dd4ac36
Irvin Lim <irvinlimweiquan@gmail.com>
2020-11-02 21:59:37 +0800

    Update seatalk.rb

--- Casks/seatalk.rb
+++ Casks/seatalk.rb
@@ -1,5 +1,6 @@
 cask "seatalk" do
     version "1.43.1"
+    sha256 "2d289f12dffd9be10015b059fb0a87ba8b262478a6a29e7d4a63eee645f069e9"
 
     url "https://static.cdn.haiserve.com/seatalk/static/client/desktop/global/darwin-x64-prod-#{version}/SeaTalk-#{version}.dmg"
     name "SeaTalk"
