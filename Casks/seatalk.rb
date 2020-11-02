1678b3082afe3fe58d40a7a17591934b95ef7783
Irvin Lim <irvinlimweiquan@gmail.com>
2020-11-02 21:56:16 +0800

    Update seatalk.rb

--- Casks/seatalk.rb
+++ Casks/seatalk.rb
@@ -1,12 +1,11 @@
 cask "seatalk" do
-    version "1.37.2"
-    sha256 "580ddc2a776d8ce92d8300a19da1d8aa0e05389da79a1a015616be3e1051e08b"
+    version "1.43.1"
 
     url "https://static.cdn.haiserve.com/seatalk/static/client/desktop/global/darwin-x64-prod-#{version}/SeaTalk-#{version}.dmg"
     name "SeaTalk"
     homepage "https://seatalk.io/"
 
-    auto_updates false
+    auto_updates true
 
     app "SeaTalk.app"
   end
